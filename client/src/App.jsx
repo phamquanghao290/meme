2f5b6ed4d5a2c60dd7548f9900aa9af129cae03f
unknown <tgdd.hieu2005@gmail.com>
2024-04-08 18:58:33 +0700

    hieu/home_footer_header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { Outlet, Routes, Route } from 'react-router-dom'
 import Cart from './page/cart/Cart'
 import NotFound from './page/notfound/NotFound'
+import Index from './page/home/Index'
+import Header from './components/Heatder/Heatder'
+import Footer from './components/Footer/Footer'
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Cart />} />
-      <Route path='*' element={<NotFound />} />
+      <Route path='/' element={<><Header /><Outlet /><Footer /></>} >
+        <Route path='/' element={<Index />} />
+        <Route path='*' element={<NotFound />} />
+        <Route path='/cart' element={<Cart />} />
+      </Route>
     </Routes>
   )
 }
---
+++ client/src/page/home/star/star.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Rate } from "antd";
+const Star = () => <Rate allowHalf defaultValue={2.5} />;
+export default Star;
